b0dd4d84dc0740cbe87e6c14b029be168ff7b965
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-01-15 09:30:00 +0500

    Learn DOM selecting by ids, classes and attributes

---
+++ javascript_codes/68_js Selecting by ids,classes etc/practice.js
@@ -0,0 +1,6 @@
+document.querySelectorAll("li").forEach(element => {
+    element.style.color="red"
+});
+
+document.querySelector(".container").firstElementChild.style.color="red"
+document.querySelector(".container").lastElementChild.style.color="green"
