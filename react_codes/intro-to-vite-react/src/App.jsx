5ccaf6bd76a76140ad78bede16d02c799c5b2d56
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-06-11 23:27:55 +0500

    Add Intro to React | Learning the folder structure

---
+++ react_codes/intro-to-vite-react/src/App.jsx
@@ -0,0 +1,10 @@
+import Biryani from "./Biryani";
+function App() {
+  return (
+    <>
+      <Biryani />
+    </>
+  );
+}
+
+export default App
---
+++ react_codes/intro-to-vite-react/src/Biryani.jsx
@@ -0,0 +1,6 @@
+function Biryani(){
+    return(
+        <h3>Yummy Biryani</h3>
+    )
+}
+export default Biryani;
