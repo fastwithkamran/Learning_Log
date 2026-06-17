a44e201ac37ff81c14192eb17c4d2abcaae1fb9f
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-06-17 15:33:14 +0500

    Built an React app using Fetch API and custom Hook

---
+++ react_codes/06_currencyConverter/src/hooks/useCurrencyinfo.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react'
+
+function useCurrencyInfo(currency) {
+
+    const [data, setData] = useState({})
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+
+            .then((result) => {
+                return result.json()
+            })
+
+            .then((result) => setData(result[currency]))
+
+    }, [currency])
+
+    return data
+}
+
+export default useCurrencyInfo
+
