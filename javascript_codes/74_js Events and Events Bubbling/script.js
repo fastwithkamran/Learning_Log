3cda48004470192e6bce39fd85e530c041127eec
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-06-19 10:30:00 +0500

    Understand Events and Events Bubbling

---
+++ javascript_codes/74_js Events and Events Bubbling/script.js
@@ -0,0 +1,11 @@
+let btn = document.getElementById("btn");
+
+btn.addEventListener("dblclick", ()=>{
+    document.querySelector(".box").innerHTML = "Button Clicked";
+})
+
+document.addEventListener("keypress", (ef)=>{
+console.log(e);
+})
+// https://www.w3schools.com/jsref/obj_mouseevent.asp
+
